b17db338c16c97c08c740b8985897e0fddb1a820
Papin Nikolai <nikolai@weirdcat.ru>
2024-03-07 15:36:06 +0300

    docker-compose now runs in dettached mode

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ git clone https://github.com/Orchestech/gamesite-frontend
 sudo docker build --no-cache -t gamesite-backend:1.0 gamesite-backend
 sudo docker build --no-cache -t gamesite-frontend:1.0 gamesite-frontend
 
-sudo docker-compose up --pull=never
+sudo docker-compose up -d --pull=never
