997674fa77eefc88b6ba11094b7d656e7625000c
Papin Nikolai <nikolai@weirdcat.ru>
2024-03-18 09:43:25 +0300

    install.sh will write new hostname into the .env file of the React application

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 git clone https://github.com/Orchestech/gamesite-backend
 git clone https://github.com/Orchestech/gamesite-frontend
+
+echo "REACT_APP_APIURL=https://gamesite.weirdcat.su" > gamesite-frontend/.env
+
 sudo docker build --no-cache -t gamesite-backend:1.0 gamesite-backend
 sudo docker build --no-cache -t gamesite-frontend:1.0 gamesite-frontend
 
