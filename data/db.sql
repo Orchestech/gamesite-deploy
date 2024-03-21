f3e4dcab8201ef2a0ab6235d939a43263a1127d4
Papin Nikolai <nikolai@weirdcat.ru>
2024-03-21 13:34:11 +0300

    Updated db.sql file

--- data/db.sql
+++ data/db.sql
@@ -1,22 +1,22 @@
 -- Database generated with pgModeler (PostgreSQL Database Modeler).
--- pgModeler version: 1.0.6
+-- pgModeler version: 1.1.0
 -- PostgreSQL version: 16.0
 -- Project Site: pgmodeler.io
 -- Model Author: ---
 
--- Database creation must be performed outside a multi-lined SQL file.
+-- Database creation must be performed outside a multi lined SQL file. 
 -- These commands were put in this file only as a convenience.
---
+-- 
 -- object: gamesite | type: DATABASE --
 -- DROP DATABASE IF EXISTS gamesite;
--- CREATE DATABASE gamesite
---	OWNER = postgres;
+CREATE DATABASE gamesite
+	OWNER = postgres;
 -- ddl-end --
 
 
 -- object: gamesite | type: SCHEMA --
 -- DROP SCHEMA IF EXISTS gamesite CASCADE;
-CREATE SCHEMA IF NOT EXISTS gamesite;
+CREATE SCHEMA gamesite;
 -- ddl-end --
 ALTER SCHEMA gamesite OWNER TO postgres;
 -- ddl-end --
@@ -25,7 +25,8 @@ SET search_path TO pg_catalog,public,gamesite;
 -- ddl-end --
 
 -- object: public.users_id_seq | type: SEQUENCE --
-CREATE SEQUENCE IF NOT EXISTS public.users_id_seq
+-- DROP SEQUENCE IF EXISTS public.users_id_seq CASCADE;
+CREATE SEQUENCE public.users_id_seq
 	INCREMENT BY 1
 	MINVALUE -2147483648
 	MAXVALUE 2147483647
@@ -37,7 +38,8 @@ CREATE SEQUENCE IF NOT EXISTS public.users_id_seq
 -- ddl-end --
 
 -- object: public.activationkeys_id_seq | type: SEQUENCE --
-CREATE SEQUENCE IF NOT EXISTS public.activationkeys_id_seq
+-- DROP SEQUENCE IF EXISTS public.activationkeys_id_seq CASCADE;
+CREATE SEQUENCE public.activationkeys_id_seq
 	INCREMENT BY 1
 	MINVALUE -2147483648
 	MAXVALUE 2147483647
@@ -49,7 +51,8 @@ CREATE SEQUENCE IF NOT EXISTS public.activationkeys_id_seq
 -- ddl-end --
 
 -- object: public.suspensions_id_seq | type: SEQUENCE --
-CREATE SEQUENCE IF NOT EXISTS public.suspensions_id_seq
+-- DROP SEQUENCE IF EXISTS public.suspensions_id_seq CASCADE;
+CREATE SEQUENCE public.suspensions_id_seq
 	INCREMENT BY 1
 	MINVALUE -2147483648
 	MAXVALUE 2147483647
@@ -61,7 +64,8 @@ CREATE SEQUENCE IF NOT EXISTS public.suspensions_id_seq
 -- ddl-end --
 
 -- object: public.queuetesters_id_seq | type: SEQUENCE --
-CREATE SEQUENCE IF NOT EXISTS public.queuetesters_id_seq
+-- DROP SEQUENCE IF EXISTS public.queuetesters_id_seq CASCADE;
+CREATE SEQUENCE public.queuetesters_id_seq
 	INCREMENT BY 1
 	MINVALUE -2147483648
 	MAXVALUE 2147483647
@@ -73,7 +77,8 @@ CREATE SEQUENCE IF NOT EXISTS public.queuetesters_id_seq
 -- ddl-end --
 
 -- object: public.activetesters_id_seq | type: SEQUENCE --
-CREATE SEQUENCE IF NOT EXISTS public.activetesters_id_seq
+-- DROP SEQUENCE IF EXISTS public.activetesters_id_seq CASCADE;
+CREATE SEQUENCE public.activetesters_id_seq
 	INCREMENT BY 1
 	MINVALUE -2147483648
 	MAXVALUE 2147483647
@@ -85,7 +90,8 @@ CREATE SEQUENCE IF NOT EXISTS public.activetesters_id_seq
 -- ddl-end --
 
 -- object: public.downloadlinks_id_seq | type: SEQUENCE --
-CREATE SEQUENCE IF NOT EXISTS public.downloadlinks_id_seq
+-- DROP SEQUENCE IF EXISTS public.downloadlinks_id_seq CASCADE;
+CREATE SEQUENCE public.downloadlinks_id_seq
 	INCREMENT BY 1
 	MINVALUE -2147483648
 	MAXVALUE 2147483647
@@ -97,7 +103,8 @@ CREATE SEQUENCE IF NOT EXISTS public.downloadlinks_id_seq
 -- ddl-end --
 
 -- object: public.profiles_id_seq | type: SEQUENCE --
-CREATE SEQUENCE IF NOT EXISTS public.profiles_id_seq
+-- DROP SEQUENCE IF EXISTS public.profiles_id_seq CASCADE;
+CREATE SEQUENCE public.profiles_id_seq
 	INCREMENT BY 1
 	MINVALUE -2147483648
 	MAXVALUE 2147483647
@@ -109,7 +116,8 @@ CREATE SEQUENCE IF NOT EXISTS public.profiles_id_seq
 -- ddl-end --
 
 -- object: public.activetesters | type: TABLE --
-CREATE TABLE IF NOT EXISTS public.activetesters (
+-- DROP TABLE IF EXISTS public.activetesters CASCADE;
+CREATE TABLE public.activetesters (
 	id integer NOT NULL DEFAULT nextval('public.activetesters_id_seq'::regclass),
 	user_id serial,
 	CONSTRAINT activetesters_pk PRIMARY KEY (id),
@@ -120,7 +128,8 @@ ALTER TABLE public.activetesters OWNER TO postgres;
 -- ddl-end --
 
 -- object: public.users | type: TABLE --
-CREATE TABLE IF NOT EXISTS public.users (
+-- DROP TABLE IF EXISTS public.users CASCADE;
+CREATE TABLE public.users (
 	id integer NOT NULL DEFAULT nextval('public.users_id_seq'::regclass),
 	username text,
 	password text NOT NULL,
@@ -136,7 +145,8 @@ ALTER TABLE public.users OWNER TO postgres;
 -- ddl-end --
 
 -- object: public.profiles | type: TABLE --
-CREATE TABLE IF NOT EXISTS public.profiles (
+-- DROP TABLE IF EXISTS public.profiles CASCADE;
+CREATE TABLE public.profiles (
 	id integer NOT NULL DEFAULT nextval('public.profiles_id_seq'::regclass),
 	user_id serial NOT NULL,
 	first_name text,
@@ -150,7 +160,8 @@ ALTER TABLE public.profiles OWNER TO postgres;
 -- ddl-end --
 
 -- object: public.queuetesters | type: TABLE --
-CREATE TABLE IF NOT EXISTS public.queuetesters (
+-- DROP TABLE IF EXISTS public.queuetesters CASCADE;
+CREATE TABLE public.queuetesters (
 	id integer NOT NULL DEFAULT nextval('public.queuetesters_id_seq'::regclass),
 	user_id serial,
 	"from" bigint NOT NULL,
@@ -165,7 +176,8 @@ ALTER TABLE public.queuetesters OWNER TO postgres;
 -- ddl-end --
 
 -- object: public.downloadlinks | type: TABLE --
-CREATE TABLE IF NOT EXISTS public.downloadlinks (
+-- DROP TABLE IF EXISTS public.downloadlinks CASCADE;
+CREATE TABLE public.downloadlinks (
 	id integer NOT NULL DEFAULT nextval('public.downloadlinks_id_seq'::regclass),
 	activetester_id serial NOT NULL,
 	link text NOT NULL,
@@ -180,11 +192,13 @@ ALTER TABLE public.downloadlinks OWNER TO postgres;
 -- ddl-end --
 
 -- object: public.activationkeys | type: TABLE --
-CREATE TABLE IF NOT EXISTS public.activationkeys (
+-- DROP TABLE IF EXISTS public.activationkeys CASCADE;
+CREATE TABLE public.activationkeys (
 	id integer NOT NULL DEFAULT nextval('public.activationkeys_id_seq'::regclass),
 	user_id serial NOT NULL,
 	key text NOT NULL,
 	force_password_change bool NOT NULL,
+	date bigint NOT NULL,
 	CONSTRAINT activationkeys_pk PRIMARY KEY (id),
 	CONSTRAINT unique_activationkeys_key UNIQUE (key),
 	CONSTRAINT unique_activationkeys_user_id UNIQUE (user_id)
@@ -194,7 +208,8 @@ ALTER TABLE public.activationkeys OWNER TO postgres;
 -- ddl-end --
 
 -- object: public.suspensions | type: TABLE --
-CREATE TABLE IF NOT EXISTS public.suspensions (
+-- DROP TABLE IF EXISTS public.suspensions CASCADE;
+CREATE TABLE public.suspensions (
 	id integer NOT NULL DEFAULT nextval('public.suspensions_id_seq'::regclass),
 	user_id serial,
 	by_user_id serial NOT NULL,
@@ -209,4 +224,52 @@ ALTER TABLE public.suspensions OWNER TO postgres;
 -- ddl-end --
 
 -- object: fk_activetesters_user_id_users | type: CONSTRAINT --
--- ALTER TABLE public.activetesters ADD CONSTRAINT IF NOT EXISTS fk_activ
+-- ALTER TABLE public.activetesters DROP CONSTRAINT IF EXISTS fk_activetesters_user_id_users CASCADE;
+ALTER TABLE public.activetesters ADD CONSTRAINT fk_activetesters_user_id_users FOREIGN KEY (user_id)
+REFERENCES public.users (id) MATCH SIMPLE
+ON DELETE NO ACTION ON UPDATE NO ACTION;
+-- ddl-end --
+
+-- object: fk_profiles_user_id_users | type: CONSTRAINT --
+-- ALTER TABLE public.profiles DROP CONSTRAINT IF EXISTS fk_profiles_user_id_users CASCADE;
+ALTER TABLE public.profiles ADD CONSTRAINT fk_profiles_user_id_users FOREIGN KEY (user_id)
+REFERENCES public.users (id) MATCH SIMPLE
+ON DELETE NO ACTION ON UPDATE NO ACTION;
+-- ddl-end --
+
+-- object: fk_queuetesters_user_id_users | type: CONSTRAINT --
+-- ALTER TABLE public.queuetesters DROP CONSTRAINT IF EXISTS fk_queuetesters_user_id_users CASCADE;
+ALTER TABLE public.queuetesters ADD CONSTRAINT fk_queuetesters_user_id_users FOREIGN KEY (user_id)
+REFERENCES public.users (id) MATCH SIMPLE
+ON DELETE NO ACTION ON UPDATE NO ACTION;
+-- ddl-end --
+
+-- object: fk_downloadlinks_activetester_id_activetesters | type: CONSTRAINT --
+-- ALTER TABLE public.downloadlinks DROP CONSTRAINT IF EXISTS fk_downloadlinks_activetester_id_activetesters CASCADE;
+ALTER TABLE public.downloadlinks ADD CONSTRAINT fk_downloadlinks_activetester_id_activetesters FOREIGN KEY (activetester_id)
+REFERENCES public.activetesters (user_id) MATCH SIMPLE
+ON DELETE NO ACTION ON UPDATE NO ACTION;
+-- ddl-end --
+
+-- object: fk_activationkeys_user_id_users | type: CONSTRAINT --
+-- ALTER TABLE public.activationkeys DROP CONSTRAINT IF EXISTS fk_activationkeys_user_id_users CASCADE;
+ALTER TABLE public.activationkeys ADD CONSTRAINT fk_activationkeys_user_id_users FOREIGN KEY (user_id)
+REFERENCES public.users (id) MATCH SIMPLE
+ON DELETE NO ACTION ON UPDATE NO ACTION;
+-- ddl-end --
+
+-- object: fk_suspensions_user_id_users | type: CONSTRAINT --
+-- ALTER TABLE public.suspensions DROP CONSTRAINT IF EXISTS fk_suspensions_user_id_users CASCADE;
+ALTER TABLE public.suspensions ADD CONSTRAINT fk_suspensions_user_id_users FOREIGN KEY (user_id)
+REFERENCES public.users (id) MATCH SIMPLE
+ON DELETE NO ACTION ON UPDATE NO ACTION;
+-- ddl-end --
+
+-- object: fk_suspensions_by_user_id_users | type: CONSTRAINT --
+-- ALTER TABLE public.suspensions DROP CONSTRAINT IF EXISTS fk_suspensions_by_user_id_users CASCADE;
+ALTER TABLE public.suspensions ADD CONSTRAINT fk_suspensions_by_user_id_users FOREIGN KEY (by_user_id)
+REFERENCES public.users (id) MATCH SIMPLE
+ON DELETE NO ACTION ON UPDATE NO ACTION;
+-- ddl-end --
+
+
